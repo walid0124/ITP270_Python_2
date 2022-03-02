a3138a1c257b44106dfa34fe32843d8f3264dd6c
walid0124 <98133034+walid0124@users.noreply.github.com>
2022-03-02 02:08:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# how to declare a variable
+# nameofTheVariable = Value
+
+
+
+
+#if decision:
+ #print('message')
+#elif condition2:
+  #print('message2')
+#else:
+  #print("another message")
+
+age_of_x = 7
+age_of_y = 9
+age_of_z = 11
+
+# >, <, >=, <=, ==, !=
+# and. or, not
+if age_of_x > age_of_y:
+    if age_of_x > age_of_z:
+      print('Oldest is x')
+    else:
+      print('Oldest is z')
+else:
+  if age_of_y > age_of_z:
+    print('Oldest is y')
+  else:
+    print('Oldest is z.')
